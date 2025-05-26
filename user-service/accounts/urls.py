c5b02dd36cea3b5619473009b9ae5ac42931d6cb
Franklyn883 <alimimianfrank@gmail.com>
2025-05-26 23:39:46 +0100

    Minor update

--- user-service/accounts/urls.py
+++ user-service/accounts/urls.py
@@ -4,10 +4,10 @@ from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="rest_register"),
-    path("login/", LoginView.as_view(), name="rest_login"),
-    path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
+    path("register", RegisterView.as_view(), name="rest_register"),
+    path("login", LoginView.as_view(), name="rest_login"),
+    path("logout", LogoutView.as_view(), name="rest_logout"),
+    path("user", UserDetailsView.as_view(), name="rest_user_details"),
+    path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh")
 ]
