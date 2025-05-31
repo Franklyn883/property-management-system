063676fd5e7dfdf032996582ca617a6ec22cdb5d
Franklyn883 <alimimianfrank@gmail.com>
2025-05-31 13:42:15 +0100

    Add endpoint for property update and delete These endpoints verify if the creator of the property is the one sending the request, before proceeding to delete or update

--- property-service/controllers/property_controller.js
+++ property-service/controllers/property_controller.js
@@ -1,29 +1,35 @@
 const Property = require("../models/property_model");
 
 const createProperty = async (req, res) => {
-   
-    try{
-        const {title,description,location, price} = req.body;
+    try {
+        const { title, description, location, price } = req.body;
 
         // Check if user info is available from verifyToken middleware
         console.log("from createProperty: ", JSON.stringify(req.user, null, 2));
 
-        if(!req.user || !req.user.pk){
-            return res.status(401).json({message: "unauthorized: User info missing"})
+        if (!req.user || !req.user.pk) {
+            return res
+                .status(401)
+                .json({ message: "unauthorized: User info missing" });
         }
         const userId = req.user.pk;
         // Create a New property
         const property = new Property({
-            title,description,location,price,user:userId // Attach the user ID from the JWT Token
-        })
-        await property.save()
+            title,
+            description,
+            location,
+            price,
+            user: userId, // Attach the user ID from the JWT Token
+        });
+        await property.save();
 
-        res.status(201).json({message: "Property created successfully", property})
-
-
-    }catch(error){
-        console.error("Error creating property:", error)
-        res.status(500).json({message: "Server Error"})
+        res.status(201).json({
+            message: "Property created successfully",
+            property,
+        });
+    } catch (error) {
+        console.error("Error creating property:", error);
+        res.status(500).json({ message: "Server Error" });
     }
 };
 
@@ -46,6 +52,71 @@ const getPropertyById = async (req, res) => {
     }
 };
 
+const deletePropertyById = async (req, res) => {
+    try {
+        const { id } = req.params.id;
+        const userId = req.user.pk;
 
+        const property = await Property.findById(id);
 
-module.exports = { createProperty, getAllProperty, getPropertyById };
+        if (!property) {
+            return res.status(404).json({ message: "Property not found" });
+        }
+
+        //check if the user is the creator of the property
+        if (property.user.toString() !== userId) {
+            res.status(403).json({
+                message: "You are not authorized to delete this property",
+            });
+        }
+
+        await Property.findByIdAndDelete(id);
+        return res
+            .status(200)
+            .json({ message: "Property deleted successfully" });
+    } catch (error) {
+        console.error("Delete Error,", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+const updatePropertyById = async (req, res) => {
+    try {
+        const { id } = req.params.id;
+        const userId = req.user.pk;
+        const updates = req.body; // The updated fields
+
+        const property = await Property.findById(id);
+
+        if (!property) {
+            return res.status(404).json({ message: "Property not found" });
+        }
+
+        //check if the user is the creator of the property
+        if (property.user.toString() !== userId) {
+            res.status(403).json({
+                message: "You are not authorized to delete this property",
+            });
+        }
+
+        const updatedProperty = await Property.findByIdAndUpdate(id, updates, {
+            new: true,
+        });
+        return res
+            .status(200)
+            .json({
+                message: "Property Updated successfully",
+                updatedProperty,
+            });
+    } catch (error) {
+        console.error("Update Error,", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+module.exports = {
+    createProperty,
+    getAllProperty,
+    getPropertyById,
+    deletePropertyById,
+    updatePropertyById,
+};
--- property-service/routes/property_routes.js
+++ property-service/routes/property_routes.js
@@ -2,6 +2,8 @@ const {
     createProperty,
     getAllProperty,
     getPropertyById,
+    deletePropertyById,
+    updatePropertyById,
 } = require("../controllers/property_controller");
 const verifyToken = require("../middleware/auth_middleware");
 const express = require("express");
@@ -12,5 +14,7 @@ const router = express.Router();
 router.post("/", verifyToken, createProperty);
 router.get("/", getAllProperty);
 router.get("/:id", getPropertyById);
-    
+router.delete("/:id", verifyToken, deletePropertyById);
+router.put("/:id", verifyToken, updatePropertyId);
+
 module.exports = router;
