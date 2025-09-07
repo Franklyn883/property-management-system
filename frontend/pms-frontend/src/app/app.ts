e4cc40e1766e03b637e381f29d6d90e2b1dbb1d8
Frank Alimimian <frank.alimimian@telosgo.com>
2025-09-07 14:17:13 +0100

    Minor update

--- frontend/pms-frontend/src/app/app.ts
+++ frontend/pms-frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./core/layout/header/header";
+import { Hero } from './core/layout/hero/hero';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header,Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
